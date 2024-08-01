30e7c8fba76533ce441358b9aa4712d105d4e39c
ChiGeorgeMofor <105949693+ChiGeorgeMofor@users.noreply.github.com>
2024-08-01 19:48:39 +0100

    Update case1.sh

--- BashScript/case1.sh
+++ BashScript/case1.sh
@@ -2,7 +2,7 @@
 # Print a message about disk useage.
 
 space_free=$( df -h | awk '{ print $5 }' | sort -n | tail -n 1 | sed 's/%//' )
-
+ 
 case $space_free in
 	[1-5]*)
 		echo Plenty of disk space available
