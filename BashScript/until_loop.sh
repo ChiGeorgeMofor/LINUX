f30f0beb5882bb9c4d61c135c1b4638c4f232200
ChiGeorgeMofor <105949693+ChiGeorgeMofor@users.noreply.github.com>
2024-08-14 07:55:00 +0100

    Update until_loop.sh

--- BashScript/until_loop.sh
+++ BashScript/until_loop.sh
@@ -3,7 +3,7 @@
  
 # The until loop is faily similar to the while loop. The difference is that it will execute the commands 
 # within it until the test becomes true.
-
+ 
 counter=1
 until [ $counter -gt 10 ]
 do
